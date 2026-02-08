3840e00e6754404e888e788d7c14d863ed8c7e13
Гарифуллин Айнур Шаукатович <garifullinas@em.mos.ru>
2026-02-08 14:05:33 +0300

    added tailwindcss

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -10,7 +10,9 @@
       class="flex flex-col gap-4 w-full sm:w-56"
     >
       <div class="flex flex-col gap-1">
-        <InputText name="username" type="text" placeholder="Username" fluid />
+        <InputText name="username" type="text" placeholder="Username" />
+        <InputText name="password" type="text" placeholder="Username" />
+
         <Message v-if="$form.username?.invalid" severity="error" size="small" variant="simple">{{
           $form.username.error?.message
         }}</Message>
