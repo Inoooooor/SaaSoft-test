1360140e8729c17a82e5e16ae38cb6b389192385
Гарифуллин Айнур Шаукатович <garifullinas@em.mos.ru>
2026-02-08 13:40:31 +0300

    Added PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
 import App from './App.vue'
+import { option } from '@primeuix/themes/aura/autocomplete'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark',
+    },
+  },
+})
 
 app.mount('#app')
