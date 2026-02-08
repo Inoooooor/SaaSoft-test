2cacf7c07490d98b87867a23f6d22d4ff36736e8
Гарифуллин Айнур Шаукатович <garifullinas@em.mos.ru>
2026-02-08 19:32:30 +0300

    handle profile type change

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -1,70 +1,80 @@
 <template>
-  <div class="card flex">
-    <Form
-      ref="formHtml"
-      v-slot="$form"
-      :initial-values="initialValues"
-      :resolver="resolver"
-      @submit="onFormSubmit"
-      class="flex gap-4 bg-gray-200"
-      :validateOnBlur="true"
-    >
-      <div class="flex flex-col gap-1">
+  <Form
+    ref="formHtml"
+    v-slot="$form"
+    :initial-values="initialValues"
+    :resolver="resolver"
+    @submit="onFormSubmit"
+    class="flex gap-1 bg-gray-200 w-200 justify-between"
+    :validateOnBlur="true"
+  >
+    <div class="flex min-w-1/2 gap-1">
+      <div class="flex flex-col gap-1 w-full">
         <ifta-label>
           <input-text
+            class="w-full"
             id="tags"
             name="tags"
             type="text"
             placeholder="XXX;MMM;YYY;III"
             @blur="$formHtml?.submit()"
+            @change=""
           />
           <label for="tags">Метки</label>
         </ifta-label>
       </div>
-      <div class="flex flex-col gap-1">
+      <div class="flex flex-col gap-1 w-full">
         <ifta-label>
           <p-select
+            class="w-full"
             id="type"
             name="type"
-            :options="['LSAP', 'Локальная']"
-            class="w-40"
-            @blur="$formHtml?.submit()"
+            :options="['LDAP', 'Локальная']"
+            @change="$form.password.value = $form.type.value === 'LDAP' ? '' : $form.password.value"
           />
           <label for="type">Тип записи</label>
         </ifta-label>
       </div>
-      <div class="flex flex-col gap-1">
-        <ifta-label>
-          <input-text name="login" type="text" placeholder="Значение" @blur="$formHtml?.submit()" />
-          <label for="login">Логин</label>
-        </ifta-label>
+    </div>
+    <div class="flex flex-col gap-1 w-full">
+      <ifta-label>
+        <input-text
+          class="w-full"
+          name="login"
+          type="text"
+          placeholder="Значение"
+          @blur="$formHtml?.submit()"
+        />
+        <label for="login">Логин</label>
+      </ifta-label>
 
-        <Message v-if="$form.login?.invalid" severity="error" size="small" variant="simple">
-          {{ $form.login.error?.message }}
-        </Message>
-      </div>
-      <div class="flex flex-col gap-1">
-        <ifta-label>
-          <password
-            id="password"
-            name="password"
-            type="password"
-            placeholder="Значение"
-            :feedback="false"
-            @blur="$formHtml?.submit()"
-          />
-          <label for="password">Пароль</label>
-        </ifta-label>
+      <Message v-if="$form.login?.invalid" severity="error" size="small" variant="simple">
+        {{ $form.login.error?.message }}
+      </Message>
+    </div>
+    <div v-if="$form?.type?.value === 'Локальная'" class="flex flex-col gap-1 w-full">
+      <ifta-label>
+        <password
+          class="w-full"
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Значение"
+          :feedback="false"
+          @blur="$formHtml?.submit()"
+        />
+        <label for="password">Пароль</label>
+      </ifta-label>
 
-        <Message v-if="$form.password?.invalid" severity="error" size="small" variant="simple">
-          {{ $form.password.error?.message }}
-        </Message>
-      </div>
-      <div>
+      <Message v-if="$form.password?.invalid" severity="error" size="small" variant="simple">
+        {{ $form.password.error?.message }}
+      </Message>
+    </div>
+    <!-- <div>
         <pre> form: {{ $form }}</pre>
-      </div>
-    </Form>
-  </div>
+      </div> -->
+  </Form>
+  <!-- </div> -->
 </template>
 
 <script setup lang="ts">
@@ -76,6 +86,10 @@ interface ProfileForm extends Omit<Profile, 'password' | 'tags'> {
   password: string
   tags: string
 }
+
+const emit = defineEmits<{
+  (e: 'update', value: Profile): void
+}>()
 
 const $formHtml = useTemplateRef<HTMLFormElement>('formHtml')
 
@@ -96,7 +110,7 @@ const data = reactive<ProfileForm>({
 const resolver = ({ values }: { values: ProfileForm }) => {
   const errors: Record<string, { message: string }[]> = {}
 
-  if (!values.password) {
+  if (values.type === 'Локальная' && !values.password) {
     errors.password = [{ message: 'Пароль обязателен.' }]
   }
 
@@ -113,5 +127,11 @@ const onFormSubmit = (e: FormSubmitEvent) => {
   if (!e.valid) return
 
   console.log('%c success submit', 'color: green')
+}
+
+const handleProfileTypeChange = (value: string) => {
+  if (value === 'LDAP') {
+    data.type = 'Локальная'
+  }
 }
 </script>
